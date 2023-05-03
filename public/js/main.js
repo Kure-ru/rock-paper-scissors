5d213dccdc66389e2018a1853b5374793edb193b
John Monk <johntmonk1@gmail.com>
2023-05-03 00:17:25 -0500

    formatted code

--- public/js/main.js
+++ public/js/main.js
@@ -15,49 +15,45 @@ if (!localStorage.getItem('botsScore')) {
   localStorage.setItem('botsScore', 0);
 }
 
-let playerScoreVal = Number(localStorage.getItem('playersScore'))
-let botScoreVal = Number(localStorage.getItem('botsScore'))
+let playerScoreVal = Number(localStorage.getItem('playersScore'));
+let botScoreVal = Number(localStorage.getItem('botsScore'));
 
-document.querySelector("#playerpoints").innerText = playerScoreVal
-document.querySelector("#botpoints").innerText = botScoreVal
-
+document.querySelector('#playerpoints').innerText = playerScoreVal;
+document.querySelector('#botpoints').innerText = botScoreVal;
 
 //originally  for the switch case statement the contents of these functions were inside the conditionals, however it looked verbose, to tidy it up I've created three functions before, and based on the result, will call the appropriate function to determine the score
-function youWin (){
-  playerScoreVal+=1,
-  localStorage.setItem('playersScore', playerScoreVal)
+function youWin() {
+  (playerScoreVal += 1), localStorage.setItem('playersScore', playerScoreVal);
 }
 
-function youLose (){
-  botScoreVal+=1
-  localStorage.setItem('botsScore', botScoreVal)
+function youLose() {
+  botScoreVal += 1;
+  localStorage.setItem('botsScore', botScoreVal);
 }
 
-function draw (){
-  console.log('you drew')
+function draw() {
+  console.log('you drew');
 }
 
 //after a game, this will update the scores without needed to refresh, because at the end of the makeReq function, this function is called.
-function updateDisplayedScores(){
-  document.querySelector("#playerpoints").innerText = playerScoreVal
-  document.querySelector("#botpoints").innerText = botScoreVal
-   //check if player or computer has 3 wins
-   checkEndGame(playerScoreVal, botScoreVal)
+function updateDisplayedScores() {
+  document.querySelector('#playerpoints').innerText = playerScoreVal;
+  document.querySelector('#botpoints').innerText = botScoreVal;
+  //check if player or computer has 3 wins
+  checkEndGame(playerScoreVal, botScoreVal);
 }
-
 
 const checkEndGame = () => {
-  if (playerScoreVal === 3){
-    document.querySelector("#result").innerHTML = "YOU WIN!!"
-    playerScoreVal = 0
-    botScoreVal = 0
+  if (playerScoreVal === 3) {
+    document.querySelector('#result').innerHTML = 'YOU WIN!!';
+    playerScoreVal = 0;
+    botScoreVal = 0;
+  } else if (botScoreVal === 3) {
+    document.querySelector('#result').innerHTML = 'GAME OVER';
+    playerScoreVal = 0;
+    botScoreVal = 0;
   }
-  else if (botScoreVal === 3){
-    document.querySelector("#result").innerHTML = "GAME OVER"
-    playerScoreVal = 0
-    botScoreVal = 0
-  }
-}
+};
 
 async function makeReq(choiceValue) {
   // const playerChoice = document.querySelectorAll("button").value; //grab the players choice
@@ -81,18 +77,18 @@ async function makeReq(choiceValue) {
     botSelectionButton.classList.remove('bot__selection');
   }, '800');
 
-  switch(data.result){
-    case "You Win":
+  switch (data.result) {
+    case 'You Win':
       youWin();
       break;
-    case "You Lose":
+    case 'You Lose':
       youLose();
       break;
-    case "Draw":
-      draw()
+    case 'Draw':
+      draw();
       break;
     default:
-      console.error ('incorrect result')
+      console.error('incorrect result');
   }
   updateDisplayedScores();
 }
